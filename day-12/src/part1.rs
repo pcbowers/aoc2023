07356a1666a8bb9c4b3c900e7796d0125bd09d4d
Chris Bowers <pcbowers@gmail.com>
2023-12-13 08:48:39 -0500

    feat: ✨ Add day 12 part 1 solution and benchmark
    part 2 works theoretically, but would take years to run. I'll need to optimize the algorithm

---
+++ day-12/src/part1.rs
@@ -0,0 +1,61 @@
+use itertools::Itertools;
+use nom::{
+    bytes::complete::{tag, take_until},
+    character::complete::{self, space1},
+    multi::separated_list1,
+    sequence::separated_pair,
+    IResult,
+};
+
+pub fn parse(input: &str) -> IResult<&str, (&str, Vec<u64>)> {
+    separated_pair(
+        take_until(" "),
+        space1,
+        separated_list1(tag(","), complete::u64),
+    )(input)
+}
+
+pub fn process(input: &str) -> String {
+    input
+        .lines()
+        .filter_map(|line| parse(line).ok())
+        .map(|(_, (springs, damaged_groups))| {
+            itertools::repeat_n(['#', '.'], springs.chars().filter(|&c| c == '?').count())
+                .multi_cartesian_product()
+                .map(|product| {
+                    let mut permutation = product.into_iter();
+                    springs
+                        .chars()
+                        .map(|c| match c {
+                            '?' => permutation.next().unwrap(),
+                            value => value,
+                        })
+                        .join("")
+                })
+                .filter(|permutation| {
+                    permutation
+                        .chars()
+                        .group_by(|c| c == &'#')
+                        .into_iter()
+                        .filter_map(|(is_damaged, section)| {
+                            is_damaged.then_some(section.count() as u64)
+                        })
+                        .collect_vec()
+                        == *damaged_groups
+                })
+                .count()
+        })
+        .sum::<usize>()
+        .to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        let result = process(include_str!("../data/example.txt"));
+        assert_eq!(result, "21".to_string());
+    }
+}
