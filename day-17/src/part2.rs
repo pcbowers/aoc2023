d42c0d03d7842a42568e04e3e16ad846589daf3e
Chris Bowers <pcbowers@gmail.com>
2023-12-17 20:56:24 -0500

    feat: ✨ Add day 17 solutions and benchmarks

---
+++ day-17/src/part2.rs
@@ -0,0 +1,171 @@
+use grid::Grid;
+use itertools::Itertools;
+use pathfinding::prelude::astar;
+
+type Point = (isize, isize);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Direction {
+    North,
+    South,
+    East,
+    West,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Vector {
+    point: Point,
+    direction: Direction,
+    straight_for: usize,
+}
+
+impl Vector {
+    pub fn derive(&self, point: Point, direction: Direction, straight_for: usize) -> Self {
+        Self {
+            point: (self.point.0 + point.0, self.point.1 + point.1),
+            direction,
+            straight_for,
+        }
+    }
+
+    pub fn successors(grid: &Grid<usize>, _: Point) -> impl Fn(&Self) -> Vec<(Self, usize)> + '_ {
+        use Direction::*;
+
+        move |vector| {
+            let mut possible_vectors: Vec<(Self, usize)> = Vec::new();
+
+            if vector.straight_for < 10 {
+                let straight_for = vector.straight_for + 1;
+                let straight_vector = match vector.direction {
+                    North => vector.derive((-1, 0), North, straight_for),
+                    South => vector.derive((1, 0), South, straight_for),
+                    East => vector.derive((0, 1), East, straight_for),
+                    West => vector.derive((0, -1), West, straight_for),
+                };
+
+                if let Some(weight) = grid.get(
+                    straight_vector.point.0 as usize,
+                    straight_vector.point.1 as usize,
+                ) {
+                    possible_vectors.push((straight_vector, *weight))
+                }
+            }
+
+            if vector.straight_for >= 4 {
+                let right_vector = match vector.direction {
+                    North => vector.derive((0, 1), East, 1),
+                    South => vector.derive((0, -1), West, 1),
+                    East => vector.derive((1, 0), South, 1),
+                    West => vector.derive((-1, 0), North, 1),
+                };
+
+                if let Some(weight) =
+                    grid.get(right_vector.point.0 as usize, right_vector.point.1 as usize)
+                {
+                    possible_vectors.push((right_vector, *weight))
+                }
+
+                let left_vector = match vector.direction {
+                    North => vector.derive((0, -1), West, 1),
+                    South => vector.derive((0, 1), East, 1),
+                    East => vector.derive((-1, 0), North, 1),
+                    West => vector.derive((1, 0), South, 1),
+                };
+
+                if let Some(weight) =
+                    grid.get(left_vector.point.0 as usize, left_vector.point.1 as usize)
+                {
+                    possible_vectors.push((left_vector, *weight))
+                }
+            }
+
+            possible_vectors
+        }
+    }
+
+    pub fn heuristic(_: &Grid<usize>, end_point: Point) -> impl Fn(&Self) -> usize {
+        move |vector| vector.point.0.abs_diff(end_point.0) + vector.point.1.abs_diff(end_point.1)
+    }
+
+    pub fn success(_: &Grid<usize>, end_point: Point) -> impl Fn(&Self) -> bool {
+        move |vector| vector.point == end_point && vector.straight_for >= 4
+    }
+}
+
+pub fn process(input: &str) -> String {
+    let grid = Grid::from_vec(
+        input
+            .lines()
+            .flat_map(|line| {
+                line.chars()
+                    .filter_map(|heat_loss| heat_loss.to_digit(10))
+                    .map(|heat_loss| heat_loss as usize)
+            })
+            .collect(),
+        input.lines().next().map_or(0, |row| row.len()),
+    );
+
+    let end_point = (grid.rows() as isize - 1, grid.cols() as isize - 1);
+
+    if let Some((_, min_heat)) = astar(
+        &Vector {
+            point: (0, 0),
+            direction: Direction::East,
+            straight_for: 0,
+        },
+        Vector::successors(&grid, end_point),
+        Vector::heuristic(&grid, end_point),
+        Vector::success(&grid, end_point),
+    ) {
+        // print_grid(&grid, &path);
+        min_heat.to_string()
+    } else {
+        "".to_string()
+    }
+}
+
+pub fn print_grid(grid: &Grid<usize>, path: &[Vector]) {
+    use Direction::*;
+    let mut new_grid = Grid::from_vec(
+        grid.clone()
+            .into_vec()
+            .iter()
+            .map(|item| item.to_string())
+            .collect_vec(),
+        grid.cols(),
+    );
+
+    path.iter().for_each(|vector| {
+        if let Some(block) = new_grid.get_mut(vector.point.0 as usize, vector.point.1 as usize) {
+            *block = match (vector.point, vector.direction) {
+                ((0, 0), _) => block.to_string(),
+                (_, North) => "^".to_string(),
+                (_, South) => "v".to_string(),
+                (_, East) => ">".to_string(),
+                (_, West) => "<".to_string(),
+            }
+        }
+    });
+
+    dbg!(new_grid
+        .iter_rows()
+        .map(|mut row| row.join(""))
+        .collect_vec());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        let result = process(include_str!("../data/example.txt"));
+        assert_eq!(result, "94".to_string());
+    }
+
+    #[test]
+    fn test_process2() {
+        let result = process(include_str!("../data/example2.txt"));
+        assert_eq!(result, "71".to_string());
+    }
+}
--- helpers/src/lib.rs
+++ helpers/src/lib.rs
@@ -1,7 +1,171 @@
+use std::marker::PhantomData;
+
+pub fn indexed_iter<T>(
+    grid: &[T],
+    (rows, cols): (usize, usize),
+) -> impl Iterator<Item = ((usize, usize), &T)> {
+    grid.iter()
+        .enumerate()
+        .map(move |(index, item)| ((index / cols, rows % index), item))
+}
+
+pub fn indexed_iter_mut<T>(
+    grid: &mut [T],
+    (rows, cols): (usize, usize),
+) -> impl Iterator<Item = ((usize, usize), &mut T)> {
+    grid.iter_mut()
+        .enumerate()
+        .map(move |(index, item)| ((index / cols, rows % index), item))
+}
+
+pub fn iter_rows<T>(
+    grid: &[T],
+    (_, cols): (usize, usize),
+) -> impl Iterator<Item = impl Iterator<Item = &T>> {
+    grid.chunks(cols).map(|chunk| chunk.iter())
+}
+
+pub fn iter_rows_mut<T>(
+    grid: &mut [T],
+    (_, cols): (usize, usize),
+) -> impl Iterator<Item = impl Iterator<Item = &mut T>> {
+    grid.chunks_mut(cols).map(|chunk| chunk.iter_mut())
+}
+
+pub fn iter_cols<T>(
+    grid: &[T],
+    (_, cols): (usize, usize),
+) -> impl Iterator<Item = impl Iterator<Item = &T>> {
+    (0..cols).map(move |col| grid.iter().skip(col).step_by(cols))
+}
+
+pub fn iter_cols_mut<T>(
+    grid: &mut [T],
+    (rows, cols): (usize, usize),
+) -> impl Iterator<Item = impl Iterator<Item = &mut T>> {
+    let ptr = grid.as_mut_ptr();
+    (0..cols).map(move |col| (0..rows).map(move |row| unsafe { &mut *ptr.add(col + row * cols) }))
+}
+
+pub struct InnerIter<'a, T> {
+    grid_pointer: *mut T,
+    rows: usize,
+    cols: usize,
+    current_col: usize,
+    current_row: usize,
+    phantom: PhantomData<&'a mut T>,
+}
+
+impl<'a, T> Iterator for InnerIter<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_row < self.rows {
+            let offset = self.current_col + self.current_row * self.cols;
+            let result = unsafe { &mut *self.grid_pointer.add(offset) };
+            self.current_row += 1;
+            Some(result)
+        } else {
+            None
+        }
+    }
+}
+
+trait GridLike<T> {
+    fn iter_cols_mut(&mut self) -> Box<dyn Iterator<Item = InnerIter<T>>>;
+}
+
+#[derive(Debug)]
+struct MyTest<T> {
+    grid: Vec<T>,
+    rows: usize,
+    cols: usize,
+}
+
+impl<T: 'static> GridLike<T> for MyTest<T> {
+    fn iter_cols_mut(&mut self) -> Box<dyn Iterator<Item = InnerIter<T>>> {
+        let ptr = self.grid.as_mut_ptr();
+        let rows = self.rows;
+        let cols = self.cols;
+
+        Box::new((0..cols).map(move |col| InnerIter {
+            grid_pointer: ptr,
+            rows,
+            cols,
+            current_col: col,
+            current_row: 0,
+            phantom: PhantomData,
+        }))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {}
+    fn iter_rows_test() {
+        let grid: Vec<usize> = vec![1, 2, 3, 4, 5, 6];
+        let sum: Vec<usize> = iter_rows(&grid, (2, 3))
+            .map(|row| row.sum::<usize>())
+            .collect();
+
+        assert_eq!(sum, vec![1 + 2 + 3, 4 + 5 + 6])
+    }
+
+    #[test]
+    fn iter_rows_mut_test() {
+        let mut grid: Vec<usize> = vec![1, 2, 3, 4, 5, 6];
+        let sum: Vec<usize> = iter_rows_mut(&mut grid, (2, 3))
+            .map(|row| {
+                row.map(|item| {
+                    *item += 1;
+                    *item
+                })
+                .sum::<usize>()
+            })
+            .collect();
+
+        assert_eq!(sum, vec![2 + 3 + 4, 5 + 6 + 7])
+    }
+
+    #[test]
+    fn iter_cols_test() {
+        let grid: Vec<usize> = vec![1, 2, 3, 4, 5, 6];
+        let sum: Vec<usize> = iter_cols(&grid, (2, 3))
+            .map(|col| col.sum::<usize>())
+            .collect();
+
+        assert_eq!(sum, vec![1 + 4, 2 + 5, 3 + 6])
+    }
+
+    #[test]
+    fn iter_cols_mut_test() {
+        let mut grid: Vec<usize> = vec![1, 2, 3, 4, 5, 6];
+        let sum: Vec<usize> = iter_cols_mut(&mut grid, (2, 3))
+            .map(|col| {
+                col.map(|item| {
+                    *item += 1;
+                    *item
+                })
+                .sum::<usize>()
+            })
+            .collect();
+
+        assert_eq!(sum, vec![2 + 5, 3 + 6, 4 + 7])
+    }
+
+    #[test]
+    fn asdf() {
+        let mut grid = MyTest {
+            grid: vec![1, 2, 3, 4, 5, 6],
+            rows: 2,
+            cols: 3,
+        };
+
+        grid.iter_cols_mut()
+            .for_each(|col| col.skip(1).for_each(|item| *item *= 10));
+
+        dbg!(grid);
+    }
 }
