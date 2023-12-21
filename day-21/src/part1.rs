c80d7d3518867cbf0af8c9ddff395d3a858707ae
Chris Bowers <pcbowers@gmail.com>
2023-12-21 11:13:55 -0500

    perf: ⚡️ Improve perf for day 21 part1 and add benchmarks for it

--- day-21/src/part1.rs
+++ day-21/src/part1.rs
@@ -1,79 +1,69 @@
-use cached::proc_macro::cached;
 use grid::Grid;
 use itertools::Itertools;
-use std::collections::BTreeSet;
+use std::collections::{BTreeSet, HashSet};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-struct Point(isize, isize);
+struct Plot(isize, isize);
 
-impl Point {
-    fn add(&self, other: &Point) -> Point {
-        Point(self.0 + other.0, self.1 + other.1)
+impl Plot {
+    fn add(&self, other: &Plot) -> Plot {
+        Plot(self.0 + other.0, self.1 + other.1)
     }
 
-    fn neighbors(&self, grid: &Grid<char>) -> BTreeSet<Point> {
-        [Point(-1, 0), Point(1, 0), Point(0, 1), Point(0, -1)]
+    fn neighbors(&self, grid: &Grid<char>) -> BTreeSet<Plot> {
+        [Plot(-1, 0), Plot(1, 0), Plot(0, 1), Plot(0, -1)]
             .iter()
-            .map(|point| self.add(point))
-            .filter(|point| matches!(grid.get(point.0, point.1), Some('.') | Some('S')))
+            .map(|plot| self.add(plot))
+            .filter(|plot| matches!(grid.get(plot.0, plot.1), Some('.') | Some('S')))
             .collect()
     }
 }
 
-impl From<(usize, usize)> for Point {
+impl From<(usize, usize)> for Plot {
     fn from((row, col): (usize, usize)) -> Self {
-        Point(row as isize, col as isize)
+        Plot(row as isize, col as isize)
     }
 }
 
+fn print_grid(grid: &Grid<char>) {
+    grid.iter_rows().for_each(|row| {
+        let row = row.collect::<String>();
+        dbg!(row);
+    });
+}
+
+fn place_plots_on_grid(grid: &mut Grid<char>, possible_plots: &HashSet<Plot>) {
+    possible_plots.iter().for_each(|plot| {
+        if let Some(cell) = grid.get_mut(plot.0, plot.1) {
+            *cell = if *cell == 'S' { 'S' } else { 'O' };
+        }
+    });
+}
+
 pub fn process(input: &str, steps: usize) -> String {
-    let grid = Grid::from(input.lines().map(|l| l.chars().collect()).collect_vec());
+    let mut grid = Grid::from(input.lines().map(|l| l.chars().collect()).collect_vec());
 
     let (starting_point, _) = grid
         .indexed_iter()
         .find(|(_, &cell)| cell == 'S')
         .expect("should have a starting point");
 
-    // dbg!("Original Grid");
-    // grid.iter_rows().for_each(|row| {
-    //     let row = row.collect::<String>();
-    //     dbg!(row);
-    // });
+    dbg!("Original Grid");
+    print_grid(&grid);
 
-    let possible_plots = garden_plots(steps, Point::from(starting_point), &grid);
-
-    // dbg!("Visited Grid");
-    // grid.iter_rows().enumerate().for_each(|(row, line)| {
-    //     let row = line
-    //         .enumerate()
-    //         .map(|(col, &cell)| {
-    //             if possible_plots.contains(&Point::from((row, col))) && cell != 'S' {
-    //                 'O'
-    //             } else {
-    //                 cell
-    //             }
-    //         })
-    //         .collect::<String>();
-    //     dbg!(row);
-    // });
-
-    possible_plots.len().to_string()
-}
-
-#[cached(key = "(usize, Point)", convert = r#"{ (steps_left, point) }"#)]
-fn garden_plots(steps_left: usize, point: Point, grid: &Grid<char>) -> BTreeSet<Point> {
-    let neighbors = point.neighbors(grid);
-
-    if steps_left == 1 {
-        return neighbors;
+    let mut possible_plots = HashSet::from([Plot::from(starting_point)]);
+    for _ in 0..steps {
+        possible_plots = possible_plots
+            .iter()
+            .flat_map(|plot| plot.neighbors(&grid))
+            .collect();
     }
 
-    neighbors
-        .iter()
-        .fold(BTreeSet::new(), |mut points, neighbor| {
-            points.extend(garden_plots(steps_left - 1, *neighbor, grid));
-            points
-        })
+    dbg!("Visited Grid");
+    place_plots_on_grid(&mut grid, &possible_plots);
+    print_grid(&grid);
+
+    possible_plots.len().to_string()
 }
 
 #[cfg(test)]
@@ -82,7 +72,7 @@ mod tests {
 
     #[test]
     fn test_process() {
-        let result = process(include_str!("../data/example.txt"), 10);
+        let result = process(include_str!("../data/example.txt"), 6);
         assert_eq!(result, "16".to_string());
     }
 }
